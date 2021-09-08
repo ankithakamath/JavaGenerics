a2ceb71b3104dd7b1673e1b934a6b5c05a2eb3e3
Ankitha Kamath <ankithakamath@196AMBBLR-Ankitha-Kamath.local>
2021-09-08 22:28:08 +0530

    The program toprint array in integer, double and character added

--- src/com/yml/generics/PrintArray.java
+++ src/com/yml/generics/PrintArray.java
@@ -2,10 +2,36 @@ package com.yml.generics;
 
 public class PrintArray {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("welcome to print array example");
-
+	public static void toPrint(Integer[] inputArray )
+	{
+		for(int element :inputArray) {
+			System.out.printf("%s", element);
+		}
+		System.out.println();
 	}
-
+	public static void toPrint(Double[] inputArray )
+	{
+		for(Double element :inputArray) {
+			System.out.printf("%s", element);
+		}
+		System.out.println();
+	}
+	
+	public static void toPrint(Character[] inputArray )
+	{
+		for(char element :inputArray) {
+			System.out.printf("%s", element);
+		}
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+	Integer[] intArray = {1,2,3,4,5};
+	Double[] doubleArray= {1.1, 2.2, 3.3, 4.4};
+	Character[] charArray = {'h','a','p','p','y'};
+	
+	PrintArray.toPrint(intArray);
+	PrintArray.toPrint(doubleArray);
+	PrintArray.toPrint(charArray);
+	}
 }
