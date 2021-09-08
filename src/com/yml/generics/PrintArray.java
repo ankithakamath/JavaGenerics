29df073d99ac294e2f69afe2c86e84f4cff917ad
Ankitha Kamath <ankithakamath@196AMBBLR-Ankitha-Kamath.local>
2021-09-08 22:26:48 +0530

    The main method to print Array program added

---
+++ src/com/yml/generics/PrintArray.java
@@ -0,0 +1,11 @@
+package com.yml.generics;
+
+public class PrintArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to print array example");
+
+	}
+
+}
